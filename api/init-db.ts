bbfdc46bdfb90b7671f918059f95f604f613cbdb
NinjaWorld1234 <n.baladna@gmil.com>
2026-01-22 00:58:46 +0200

    Fix bcrypt import for ESM and add diagnostic buffer

--- api/init-db.ts
+++ api/init-db.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { sql } from '@vercel/postgres';
-import bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcryptjs';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
